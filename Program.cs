38219512885381fd2623930e550005723d5cd1ba
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-31 01:06:33 +0530

    Added:EmployeeWage program file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee\r\nWage Computation\r\nProgram");
+
+        }
+    }
+}
