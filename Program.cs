a23410cc903b5b8b8ffa48506ddd078e5726ed65
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-31 01:15:48 +0530

    Added: UC1 EmployeeWage program file impliment of checking employee is present or absent

--- Program.cs
+++ Program.cs
@@ -6,8 +6,21 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee\r\nWage Computation\r\nProgram");
+            Console.WriteLine("Welcome to Employee\r\nWage Computation\r\nProgram\n");
 
+           Random rnd = new Random();
+            int check = rnd.Next(0,2);
+            if (check == 0) 
+            {
+                Console.WriteLine("Employee is absent ");
+
+            }
+            else 
+            {
+                Console.WriteLine("Employee is present ");
+            }
+
+        
         }
     }
 }
